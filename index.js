5a2b01c24dbe373482d621ac067b3f9e7a4cbab1
Piyush <piyushgupta26081999@gmail.com>
2023-12-02 00:50:50 +0530

    update

---
+++ index.js
@@ -0,0 +1,24 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    items: 1,
+    margin: 60,
+    nav: true,
+    autoplay: true,
+    autoplaySpeed: 1400,
+    autoplayTimeout: 2100,
+    stagePadding: 19,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        },
+        1400: {
+            items: 1
+        }
+    }
+})
